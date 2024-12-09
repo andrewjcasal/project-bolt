fea17232784e1c06239bf0b3902f92b647b24dd9
Andrew Casal <andrewcasal@Andrews-MacBook-Pro.local>
2024-12-08 20:36:43 -0500

    update front end CORS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CONTEXT === 'production' ? 'https://peppy-cassata-b91b8e.netlify.app' : 'http://localhost:8888',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
